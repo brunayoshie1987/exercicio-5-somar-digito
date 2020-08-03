04c680c9f5be2eaaaf5329e83bd1ddfcf5abd5fa
unknown <bruna.yoshie@iventis.com.br>
2020-08-03 07:48:26 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercicio_5___Somar_Digitos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro");
+            var inteiro = Convert.ToInt32(Console.ReadLine());
+
+            var resto = 0.0;
+            var divisor = 10.0;
+            var soma = 0.0;
+            var intDivisao = 1;
+
+            while (intDivisao != 0)
+            {
+                resto = inteiro % divisor;
+                intDivisao = Convert.ToInt32(Math.Truncate(inteiro / divisor));
+
+                soma += resto;
+
+                inteiro = intDivisao;
+            }
+
+            Console.WriteLine("O resultado da soma dos dígitos é: " + soma);
+
+            Console.ReadKey();
+        }
+    }
+    }
+
